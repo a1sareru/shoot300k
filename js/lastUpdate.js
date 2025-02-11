be9ce7587be62ee6a57f79545f289bf6859f23cf
user_name <example@example.com>
2025-02-11 09:36:59 +0000

    fix: use correct id for repo-last-updated

--- js/lastUpdate.js
+++ js/lastUpdate.js
@@ -4,9 +4,9 @@ async function fetchRepoLastUpdated() {
         if (!response.ok) throw new Error(`请求失败: ${response.status}`);
 
         const lastUpdated = await response.text(); // Assuming the file contains the last updated date as text
-        document.getElementById("data-last-updated").textContent = `※ 本站最后更新：${lastUpdated}`;
+        document.getElementById("repo-last-updated").textContent = `※ 本站最后更新：${lastUpdated}`;
     } catch (error) {
-        document.getElementById("data-last-updated").textContent = "※ 获取失败";
+        document.getElementById("repo-last-updated").textContent = "※ 获取失败";
         console.error("获取主工作流最后更新时间失败:", error);
     }
 }
